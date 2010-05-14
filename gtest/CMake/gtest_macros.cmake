542a42096109970d425e633577fe37f2b0756977
Meng ZHU <meng@g.clemson.com>
2010-05-14 15:48:10 -0400

    CMakeLists.txt for gtest

--- gtest/CMake/gtest_macros.cmake
+++ gtest/CMake/gtest_macros.cmake
@@ -7,17 +7,20 @@ MACRO(GTEST_CXX
                        LINK_FLAGS      ${LINK_FLAGS}
                        COMPILE_DEFINITIONS "${DEFINITIONS}")
  target_link_libraries(${TARGET_NAME} ${LINK_LIBS})
- add_test(${TARGET_NAME} ${TARGET_NAME})
+ add_test(${TARGET_NAME} ${EXECUTABLE_OUTPUT_PATH}/${TARGET_NAME})
 endmacro(GTEST_CXX)
 
 # Generate Python tests
 MACRO(GTEST_PYTHON
-      PYTHON_SCRIPT TARGET_NAME SOURCES CXX_FLAGS LINK_FLAGS LINK_LIBS DEFINITIONS)
+      TARGET_NAME SOURCES CXX_FLAGS LINK_FLAGS LINK_LIBS PYTHON_SCRIPT)
  add_executable(${TARGET_NAME} ${SOURCES})
  set_target_properties(${TARGET_NAME}  PROPERTIES
                        CMAKE_CXX_FLAGS ${CXX_FLAGS}
-                       LINK_FLAGS      ${LINK_FLAGS}
-                       COMPILE_DEFINITIONS "${DEFINITIONS}")
+                       LINK_FLAGS      ${LINK_FLAGS})
  target_link_libraries(${TARGET_NAME} ${LINK_LIBS})
- add_test(${TARGET_NAME} ${PYTHON_EXECUTABLE} ${PYTHON_SCRIPT})
+ add_test(${TARGET_NAME}
+          ${PYTHON_EXECUTABLE}
+          ${CMAKE_CURRENT_SOURCE_DIR}/${PYTHON_SCRIPT})
+ execute_process(COMMAND ${CMAKE_COMMAND} -E copy_if_different
+                 ${CMAKE_CURRENT_SOURCE_DIR}/${PYTHON_SCRIPT} ${EXECUTABLE_OUTPUT_PATH})
 endmacro(GTEST_PYTHON)
